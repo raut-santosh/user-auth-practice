bb01c327a409b8f8bd0b05ae1093e5a92b16ac87
santosh raut <rautsantoshtukaram@gmail.com>
2024-05-10 12:15:12 +0530

    crud user login

--- app.js
+++ app.js
@@ -16,30 +16,119 @@ app.use(session({
 
 app.set('views', path.join(__dirname, 'views'))
 
+
+function requireLogin(allowedRoles) {
+    return async function(req, res, next) {
+      if (req.session && req.session.userId) {
+        const user = await prisma.users.findUnique({
+          where: {
+            id: req.session.userId
+          }
+        });
+        if (!user || !allowedRoles.includes(user.role)) {
+          return res.redirect('/login');
+        }
+        req.user = user;
+        next();
+      } else {
+        return res.redirect('/login');
+      }
+    };
+  }
+  
+  
+
+
+
 app.get('/', (req, res) => {
-    res.redirect('/user');
+    res.redirect('/admin');
 });
 
-app.get('/login', (req, res) => {
+app.get('/login', async (req, res) => {
     res.render('login')
 })
 
+app.post('/login', async (req, res) => {
+    const user = await prisma.users.findUnique({
+        where: {
+          email: req.body.email,
+        },
+      })
+    if(user.password === req.body.password){
+        req.session.userId = user.id;
+        res.redirect('/'); 
+    }else{
+        res.redirect('login')
+    }
+})
 
-app.get('/admin',async (req, res) => {
-    res.render('admin', { username: req.session.username });
+app.get('/logout', (req,res) => {
+    req.session.destroy((err) => {
+        if (err) {
+          return res.redirect('/'); // Handle error
+        }
+        res.redirect('/login'); // Redirect to the login page after successful logout
+      });
+})
+
+
+app.get('/admin', requireLogin(['admin']), async (req, res) => {
+    const users = await prisma.users.findMany();
+    console.log(users)
+    res.render('admin', { users });
 });
 
 
-app.get('/user', (req, res) => {
+app.get('/user', requireLogin(['admin', 'user']), (req, res) => {
     res.render('user')
 })
 
-app.get('/create-user', (req, res) => {
-    res.render('admin/create-user')
+app.get('/create-user', requireLogin(['admin','user']), (req, res) => {
+    const user = {name: '', email: '', role: '', password: ''}
+    res.render('admin/create-user', {user} )
+})
+
+app.post('/create-user', requireLogin(['admin','user']), async (req, res) => {
+    const user = await prisma.users.create({
+        data: {
+          email: req.body.email,
+          name: req.body.name,
+          role: req.body.role,
+          password: req.body.password
+        },
+    })
+    res.render('admin/edit-user', user)
+})
+
+app.get('/edit-user/:id',  requireLogin(['admin','user']), async (req, res) =>{
+    const user = await prisma.users.findUnique({
+        where: {
+          id: Number(req.params.id),
+        },
+      })
+    console.log(user)
+    res.render('admin/edit-user', {user})
+})
+
+app.post('/edit-user/:id', requireLogin(['admin','user']), async (req, res) => {
+    const user = await prisma.users.update({
+        where: {
+          id: Number(req.params.id),
+        },
+        data: {
+            name: req.body.name,
+            role: req.body.role,
+            email: req.body.email,
+            password: req.body.password
+        },
+      })
+      console.log(user)
+      res.redirect(`/edit-user/${user.id}`)
+
 })
 
 
-app.get('/assistant', (req, res) => {
+app.get('/assistant',requireLogin(['admin','assistant']), (req, res) => {
     res.render('assistant', { username: req.session.username });
 });
 
